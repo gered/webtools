457afba98910b4daafb02cbf05480f3b1406fd40
gered <gered@blarg.ca>
2016-01-25 13:06:03 -0500

    update dependencies

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license     {:name "MIT License"
                 :url  "http://opensource.org/licenses/MIT"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.3.1" :scope "provided"]
-                 [ring/ring-core "1.3.1" :scope "provided"]
-                 [cheshire "5.3.1"]
-                 [prismatic/schema "0.3.3"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.4.0" :scope "provided"]
+                 [ring/ring-core "1.4.0" :scope "provided"]
+                 [cheshire "5.5.0"]
+                 [prismatic/schema "1.0.4"]])
