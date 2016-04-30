c2a0531f2dcb5365f095d047ba8e0d9288578acc
gered <gered@blarg.ca>
2016-04-30 16:17:04 -0400

    version 0.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gered/webtools "0.0.1"
+(defproject gered/webtools "0.0.2"
   :description "Miscellaneous helper functions and macros for Ring and Compojure."
   :url         "https://github.com/gered/webtools"
   :license     {:name "MIT License"
